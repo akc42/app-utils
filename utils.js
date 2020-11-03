5b3b685ae829ecde8834cd3738d01f7fa15d3767
Alan Chandler <alan@chandlerfamily.org.uk>
2020-11-03 16:11:08 +0000

    Include global config promise,  master tab promise and debug

--- utils.js
+++ utils.js
@@ -408,3 +408,147 @@ export class AppKeys {
     }
   }
 }
+
+class MasterClose extends Event {
+  /*
+     This event is fired on the window when a master tab closes.  Its for the home page to re check the master
+     promise to see if made it.
+
+  */
+  constructor() {
+    super('master-close', { composed: true, bubbles: true });
+  }
+};
+
+let master = false;
+let masterResolver;
+export let masterPromise = new Promise(resolve => masterResolver = resolve);
+
+
+let otherTabs = new Set();
+let timer = 0.
+const tabId = Date.now().toString();
+const storageHandler = (e) => {
+  if (e.key === 'pageOpen') {
+    otherTabs.add(localStorage.getItem('pageOpen'))
+    localStorage.setItem('pageAvailable', tabId); //respond - so the ender knows he is not master.
+  }
+  if (e.key === 'pageAvailable') {
+    if (timer > 0) {
+      clearTimeout(timer);
+      master = false;
+      masterResolver(false);
+    }
+  }
+  if (e.key === 'pageClose') {
+    const closer = JSON.parse(localStorage.getItem('pageClose'));
+    if (master) {
+      otherTabs.delete(closer.id);
+    } else {
+      if (closer.master) {
+        otherTabs = new Set(closer.list); //Make a set from the list the closing master had
+        otherTabs.delete(tabId); //remove self  
+        masterPromise = new Promise(resolve => masterResolver = resolve);
+
+        if (closer.size === 1) {
+          //must be just me left, so I become master
+          master = true;
+          masterResolver(true);
+        } else {
+          //We need to wait a random time before trying to become master, but
+          timer = setTimeout(() => {
+            timer = setTimeout(() => {
+              master = true;
+              masterResolver(true);
+            }, 70); //wait 70 ms to see if our claim was refuted
+            localStorage.setItem('pageClaim', tabId); //try and claim storage
+          }, 70 * Math.floor((Math.random() * 40))); //wait random time between 70ms and about 3 seconds before trying to claim master
+        }
+        window.dispatchEvent(new MasterClose());
+      }
+    }
+  }
+  if (e.key === 'pageClaim') {
+    if (timer > 0) {
+      clearTimeout(timer); //someone else has claimed master ship, kill of our attempt;
+      master = false;
+      masterResolver(false);
+    }
+  }
+}
+
+window.addEventListener('storage', storageHandler);
+const unloadHandler = () => {
+  localStorage.setItem('pageClose', JSON.stringify({
+    master: master,
+    id: tabId,
+    size: otherTabs.size,
+    list: Array.from(otherTabs)
+  }));
+  window.removeEventListener('storage', storageHandler);
+  window.removeEventListener('beforeunload', unloadHandler);
+};
+window.addEventListener('unload', unloadHandler);
+timer = setTimeout(() => {
+  timer = 0;  //prevent out assertion being overridden by a later try
+  master = true;
+  masterResolver(true);
+}, 70);
+localStorage.setItem('pageOpen', tabId);
+
+export const configPromise = new Promise(resolve => {
+  window.fetch('/api/config', { method: 'get' }).then(response => response.text()).then(text => {
+    try {
+      return JSON.parse(text);
+    } catch (err) {
+      //we failed to parse the json - the actual code should be in the text near the end;
+      throw new ApiError(parseInt(text.substr(-6, 3), 10));
+    }
+  }).then(conf => { //most like just update values.
+    for(const p in conf ) {
+      sessionStorage.setItem(p, conf[p]);
+    }
+    resolve();
+  }).catch((e) => {
+    window.dispatchEvent(new Error(e));
+    resolve()
+  });
+});
+export function Debug(t) {
+  if (typeof t !== 'string' || t.length === 0 || t.toUpperCase() === 'ALL') {
+    console.error('Debug requires topic which is a non zero length string which is not "ALL"', t, 'Received');
+    throw new Error('Debug requires a non zero length string which is not "ALL"');
+  }
+  const topic = t.toLowerCase(); //need to store it for the closure use.
+  let timestamp = new Date().getTime();
+  let using = false; //needs to be in close as its unique to this instance.
+  let clientUid = 0;
+  configPromise.then(() => {
+    const clientLog = sessionStorage.getItem('clientLog')
+    clientUid = sessionStorage.getItem('clientUid');
+
+    using = ((clientLog === null && topic === 'logger' ) || 
+              clientLog === 'ALL' || 
+              clientLog.indexOf(`:${topic}:`) >= 0); //always the same 
+  });
+  return function (...args) {
+    let uid = 0;
+    if (clientUid !== null && clientUid !== 0) {
+      const userjson = sessionStorage.getItem('user');
+      uid = userjson === null ? 0 : (JSON.parse(userjson)).uid;
+    }
+    if (using || (uid !== 0 && uid === clientUid)) {
+      const message = args.reduce((cum, arg) => {
+        return `${cum} ${arg}`.trim();
+      }, '');
+      const now = new Date().getTime();
+      const gap = now - timestamp;
+      timestamp = now;
+      console.log(`+${gap}ms`, topic, message);
+      const logpath = `/api/log/${encodeURIComponent(topic)}/${encodeURIComponent(message)}/${encodeURIComponent(gap)}`
+
+      window.fetch(logpath, { method: 'post' })
+        .catch(() => true);//no interest in reply, but must make sure it doesn't throw an exception.
+    }
+  }
+}
