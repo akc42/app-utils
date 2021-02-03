330fb9461923b2574f542790f53a12b7b4197688
Alan Chandler <alan@chandlerfamily.org.uk>
2021-02-03 17:59:36 +0000

    Undo previous changes

--- utils.js
+++ utils.js
@@ -74,14 +74,9 @@ export async function api(url, params, blob) {
       return JSON.parse(text);
     }
   } catch (err) {
-    const e = err;
-    //we do this to escape the promise chain
-    setTimeout(() => {
-      if (e.type === 'api-error') throw e; //just throw whatever error we had
-      //we failed to parse the json - the actual code should be in the text near the end;
-      throw new CustomEvent('api-error', { composed: true, bubbles: true, details: parseInt(text.substr(-6, 3), 10) });
-    });
-    return {}; //just sumulate a null response
+    if (err.type === 'api-error') throw err; //just throw whatever error we had
+    //we failed to parse the json - the actual code should be in the text near the end;
+    throw new CustomEvent('api-error', { composed: true, bubbles: true, details: parseInt(text.substr(-6, 3), 10) });
   }
 }
 
